e28fad92e9a0fd6944e35dc3fe6bd6750dc8bdf2
jamtechba <jamtechba@gmail.com>
2026-01-23 13:50:02 +0530

    change seed file

--- scripts/seed-database.ts
+++ scripts/seed-database.ts
@@ -1,8 +1,5 @@
+import "dotenv/config";
 import "reflect-metadata";
-import { config } from "dotenv";
 import { seedData } from "../app/database/migrations/seed-data";
-
-// Load environment variables
-config();
 
 seedData();
